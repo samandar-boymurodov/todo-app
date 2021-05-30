ff0ffdf8b0053b4b297f696664d13105677a9397
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-31 00:45:12 +0500

    Basic Setup with Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./UI/Header";
+import Register from "./UI/Register";
+import Login from "./UI/Login";
+import ToDoGroupsContainer from "./Components/ToDoGroupsContainer";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/dashboard" component={ToDoGroupsContainer} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route component={() => <>Not Found!</>} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/ToDoGroupContainer.js
@@ -0,0 +1,3 @@
+export default function TodoGroupContainer() {
+  return <>TodoGroupContainer</>;
+}
